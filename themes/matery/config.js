1d4265b72453a2aec93c0339fbc5494bae34e7aa
Hachiu <95755838+Xc0417@users.noreply.github.com>
2025-08-30 21:53:05 +0800

    Update config.js

--- themes/matery/config.js
+++ themes/matery/config.js
@@ -20,14 +20,14 @@ const CONFIG = {
   MATERY_MENU_ARCHIVE: true, // 显示归档
   MATERY_MENU_SEARCH: true, // 显示搜索
 
-  MATERY_POST_LIST_COVER: true, // 文章封面
+  MATERY_POST_LIST_COVER: false, // 文章封面
   MATERY_POST_LIST_SUMMARY: true, // 文章摘要
   MATERY_POST_LIST_PREVIEW: true, // 读取文章预览
 
   MATERY_ARTICLE_ADJACENT: true, // 显示上一篇下一篇文章推荐
-  MATERY_ARTICLE_COPYRIGHT: true, // 显示文章版权声明
+  MATERY_ARTICLE_COPYRIGHT: false, // 显示文章版权声明
   MATERY_ARTICLE_NOT_BY_AI: false, // 显示非AI写作
-  MATERY_ARTICLE_RECOMMEND: true, // 文章关联推荐
+  MATERY_ARTICLE_RECOMMEND: false, // 文章关联推荐
 
   MATERY_WIDGET_LATEST_POSTS: true, // 显示最新文章卡
   MATERY_WIDGET_ANALYTICS: false, // 显示统计卡
